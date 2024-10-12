b9e0f85fabd7f6f2dd1989b8525b11bdba5ffee1
librasvii <moreiralucas511@gmail.com>
2024-10-12 09:14:15 -0300

    update

--- data_structs/insertion_sort/v1.rs
+++ data_structs/insertion_sort/v1.rs
@@ -1,18 +1,17 @@
 fn main() {
-    let array = [2, 4, 1, 5, 3];
-    let iter = array.iter();
-    let j = 2;
-    let i = 1;
+    let array = vec![2, 4, 1, 5, 3];
+    let sorted_array = insertion_sort(array);
+    println!("{:?}", sorted_array);
+}
 
-    for j in iter {
-        let key = array[j];
+fn insertion_sort(mut array: Vec<i32>) -> Vec<i32> {
+    for i in 1..array.len() {
+        let mut j = i;
 
-        i = j - 1;
-
-        while i > 0 && array[i] > key {
-            array[i + 1] = array[i];
-            i = i - 1;
+        while j > 0 && array[j] < array[j - 1] {
+            array.swap(j, j - 1);
+            j -= 1;
         }
-        array[i + 1] = key;
     }
+    array
 }
